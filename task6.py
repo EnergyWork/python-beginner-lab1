50881e048d2b41e2e8c36714c40e6172a8f3a6c0
Artyom <ag.energy@mail.ru>
2020-09-12 02:10:53 +0300

    upd

--- task6.py
+++ task6.py
@@ -3,13 +3,9 @@ def task6():
         #6 Числа Фибоначчи
     """
     n = int(input())
-    #i = 0
-    fib = [1, 1]
-    for _ in range(n):
+    fib = [0, 1]
+    for _ in range(n - 2):
         fib.append(fib[-1] + fib[-2])
-    #while i < n - 2:
-    #    fib.append(fib[-1] + fib[-2])
-    #    i += 1
     print(fib)
 
 if __name__ == "__main__":
--- task8.py
+++ task8.py
@@ -2,9 +2,14 @@ def task8():
     """
         #8 Все трехзначные числа Армстронга
     """
+    #способ 1
     for num in range(100, 1000, 1):
         if num == sum([int(x)**3 for x in str(num)]):
             print(num)
+
+    #способ 2
+    mas = list(filter(lambda x: x != 0, [num if num == sum([int(x)**3 for x in str(num)]) else 0 for num in range(100, 1000, 1)]))
+    print(mas)
 
 if __name__ == "__main__":
     task8() # Все трехзначные числа Армстронга
